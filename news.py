f22aff6bc7dc4ed5b650a653b41c90a5b84e9d87
Spidey03 <sayhai2naveenkumar@gmail.com>
2021-06-29 10:01:53 +0530

    get sources print

--- news.py
+++ news.py
@@ -1,7 +1,8 @@
+import collections
 import datetime
 import json
 import textwrap
-from typing import Union
+from typing import Union, List
 
 from newsapi import NewsApiClient
 
@@ -43,12 +44,16 @@ class NewsSearch:
 
     def get_sources(
             self, category: str = None,
-            language: str = "en", country: str = None
+            language: str = "en", country: str = None,
+            enable_description: bool = False
     ):
         sources = self.news_api.get_sources(
             category=category, language=language, country=country
         )
-        self.print_sources(sources=sources["sources"])
+        self.print_sources(
+            sources=sources["sources"],
+            enable_description=enable_description
+        )
 
     @property
     def api_key(self):
@@ -57,7 +62,7 @@ class NewsSearch:
 
     def print_news(self, news):
         print("Total Results Found: {}".format(news["totalResults"]))
-        print(json.dumps(news, indent=4))
+        # print(json.dumps(news, indent=4))
         for news_item in news["articles"]:
             self._print_decoration()
             self._print_title(news_item)
@@ -68,11 +73,20 @@ class NewsSearch:
             self._print_decoration()
             self._print_new_line()
 
-    def print_sources(self, sources):
+    def print_sources(self, sources: List, enable_description: bool):
+        print("Total Sources are: {}", len(sources))
+        source_map = collections.defaultdict()
+        counter = 1
         for source in sources:
-            self._print_decoration()
-            self._print_name(name=source.get("name"))
-            self._print_decoration()
+            source_map[str(counter)] = source
+            self._print_name(name=source.get("name"), counter=counter)
+            if source.get("description") and enable_description:
+                self._print_source_description(description=source.get("description"))
+            else:
+                self._print_new_line()
+            counter += 1
+        # print(json.dumps(sources, indent=4))
+        return source_map
 
     @staticmethod
     def _print_url(news_item):
@@ -114,7 +128,7 @@ class NewsSearch:
 
     def _print_title(self, news_item):
         title = textwrap.shorten(news_item.get("title", ""), self.width)
-        print("| {}{:<150}{:>} ".format(Colors.HEADER, title, Colors.ENDC))
+        print("| {}{}{:<150}{:>}{} ".format(Colors.BOLD, Colors.HEADER, title, Colors.ENDC, Colors.ENDC))
 
     def _print_decoration(self):
         print("{:<} {:} {:>}".format('+', '-' * self.width, '+'))
@@ -129,10 +143,22 @@ class NewsSearch:
             published_at = news_item["publishedAt"]
             print("| {}{:>150}{:>} ".format(Colors.WHITE, published_at, Colors.ENDC))
 
-    def _print_name(self, name):
-        pass
+    @staticmethod
+    def _print_name(name: str, counter: int):
+        print("{}{}{}. {}{}{} ".format(Colors.HEADER, Colors.BOLD, counter, name, Colors.ENDC, Colors.ENDC), end=" ")
+
+    @staticmethod
+    def _print_source_description(description):
+        w = textwrap.TextWrapper(
+            width=120,
+            max_lines=1,
+            subsequent_indent="| ",
+            fix_sentence_endings=True
+        )
+        description = w.fill(description)
+        print("- {}{:<100}{:>} ".format(Colors.WHITE, description, Colors.ENDC))
 
 
 if __name__ == "__main__":
     news = NewsSearch()
-    news.get_sources()
+    news.get_top_headlines()
